c7c8bfb8195c714e49e08d398c580ac0f81e1ab6
Dave Dietrick <djdietrick@gmail.com>
2020-08-12 16:18:55 -0400

    Initialized project and firebase

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCoTRxagOnK0M_Tizp8220s3mcX3itpCgg",
+  authDomain: "dash-homepage-b17fb.firebaseapp.com",
+  databaseURL: "https://dash-homepage-b17fb.firebaseio.com",
+  projectId: "dash-homepage-b17fb",
+  storageBucket: "dash-homepage-b17fb.appspot.com",
+  messagingSenderId: "1005228845792",
+  appId: "1:1005228845792:web:57e57d4a6139fe85ae3e85",
+  measurementId: "G-9HNWW7JZT2"
+});
+
+export const db = firebase.firestore();
 
 new Vue({
-  router,
   store,
   render: function (h) { return h(App) }
 }).$mount('#app')
